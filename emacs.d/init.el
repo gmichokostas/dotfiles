40d11a5e6c15abd1b5e82b97b6143a2dd3fe65d8
George Michokostas <michokostas@gmail.com>
2018-07-24 21:08:29 +0300

    Emacs
    - use https for package-archives
    - replace tabs with whitespaces
    - re-organise init.el
    - install ruby-electric and magit packages
    - ido changes
    - add functionality for open line below and above the current line
    - display redundant whitespaces

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,8 +1,8 @@
 ;; Package configs
 (require 'package)
 (setq package-enable-at-startup nil)
-(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
-                         ("gnu"   . "http://elpa.gnu.org/packages/")
+(setq package-archives '(("org"   . "https://orgmode.org/elpa/")
+                         ("gnu"   . "https://elpa.gnu.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
@@ -13,6 +13,7 @@
 
 (eval-when-compile (require 'use-package))
 
+;; MacOS related
 (when (eq system-type 'darwin)
   (exec-path-from-shell-initialize)
   ;; titlebar for MacOS
@@ -21,35 +22,40 @@
   (setq ns-use-proxy-icon  nil)
   (setq frame-title-format nil)
   (setq mac-option-key-is-meta nil ;; rebind Meta key to cmd
-	mac-command-key-is-meta t
-	mac-command-modifier 'meta
-	mac-option-modifier 'none))
+        mac-command-key-is-meta t
+        mac-command-modifier 'meta
+        mac-option-modifier 'none))
 
 ;; general editor settings
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 (global-auto-revert-mode 1) ;; reload file when it has changed in the disk
 
+;; Better defaults
+(setq
+  column-number-mode t
+  display-line-numbers-grow-only t
+  global-hl-line-mode t
+  indent-tabs-mode nil
+  initial-scratch-message ""
+  load-prefer-newer t
+  ring-bell-function 'ignore
+  select-enable-clipboard t
+  inhibit-startup-screen t)
+
 ;; UI
 (tool-bar-mode -1)
 (tooltip-mode -1)
 (scroll-bar-mode -1)
-(set-frame-font "Droid Sans Mono for Powerline 14")
-(setq column-number-mode t)
-(setq display-line-numbers-grow-only t)
-(setq inhibit-startup-message t)
-(setq ring-bell-function 'ignore)
+(set-frame-font "Roboto Mono for Powerline 14")
 (show-paren-mode)
-(global-display-line-numbers-mode t)
-(global-hl-line-mode t)
-(set-face-background 'hl-line "gray16")
 
 ;; packages
 
-(use-package tangotango-theme
+(use-package doom-themes
   :ensure t
   :config
-  (load-theme 'tangotango t))
+  (load-theme 'doom-nord t))
 
 (use-package cider
   :ensure t
@@ -59,21 +65,25 @@
   :ensure t
   :mode (("\\.clj\\'" . clojure-mode)
          ("\\.edn\\'" . clojure-mode)
-	 ("\\.cljs\\'" . clojurescript-mode)))
+         ("\\.cljs\\'" . clojurescript-mode)))
 
 (use-package flymake-ruby
   :ensure t
   :hook (ruby-mode . flymake-ruby-load))
 
+(use-package ruby-electric
+  :ensure t
+  :hook (ruby-mode . ruby-electric-mode))
+
 (use-package rainbow-delimiters
   :ensure t
   :hook ((prog-mode . rainbow-delimiters-mode)
-	 (cider-repl-mode . rainbow-delimiters-mode)))
+         (cider-repl-mode . rainbow-delimiters-mode)))
 
 (use-package smartparens
   :ensure t
   :hook ((prog-mode . smartparens-mode)
-	 (cider-repl-mode . smartparens-mode)))
+         (cider-repl-mode . smartparens-mode)))
 
 (use-package which-key
   :ensure t
@@ -90,7 +100,7 @@
 (use-package multiple-cursors
   :ensure t
   :bind (("M-." . mc/mark-next-like-this-symbol)
-	 ("M-," . mc/mark-previous-like-this-symbol))
+         ("M-," . mc/mark-previous-like-this-symbol))
   :hook (prog-mode . multiple-cursors-mode))
 
 (use-package web-mode
@@ -101,41 +111,44 @@
   (setq web-mode-enable-current-column-highlight t)
   (setq web-mode-enable-current-element-highlight t)
   :mode (("\\.html?\\'" . web-mode)
-	 ("\\.erb\\'" . web-mode)))
+         ("\\.erb\\'" . web-mode)))
 
-;; use shift arrow to navigate from window to windown
+;; use shift arrow to navigate from window to window
 (windmove-default-keybindings)
 
 ;; aliases
 (defalias 'list-buffers 'ibuffer)
 (defalias 'yes-or-no-p 'y-or-n-p)
+(defalias 'eb 'eval-buffer)
 
 ;; disable auto-save and auto-backup
 (setq auto-save-default nil)
 (setq make-backup-files nil)
 
 ;; ido
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
+(setq ido-enable-flex-matching t
+      ido-everywhere t
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess)
 (ido-mode 1)
 
 ;; move lines around
 (defmacro save-column (&rest body)
   `(let ((column (current-column)))
      (unwind-protect
-         (progn ,@body)
+       (progn ,@body)
        (move-to-column column))))
 (put 'save-column 'lisp-indent-function 0)
 
 (defun move-line-up ()
-  "Move the current line up one line"
+  "Move the current line up one line."
   (interactive)
   (save-column
     (transpose-lines 1)
     (forward-line -2)))
 
 (defun move-line-down ()
-  "Move the current line down one line"
+  "Move the current line down one line."
   (interactive)
   (save-column
     (forward-line 1)
@@ -160,6 +173,7 @@
   "Move the current line up by N lines."
   (interactive "r\np")
   (move-region start end (if (null n) -1 (- n))))
+
 (defun move-region-down (start end n)
   "Move the current line down by N lines."
   (interactive "r\np")
@@ -168,5 +182,34 @@
 (global-set-key (kbd "M-C-<up>") 'move-region-up)
 (global-set-key (kbd "M-C-<down>") 'move-region-down)
 
-;; paste from clipboard
-(global-set-key (kbd "M-p") 'clipboard-yank)
+(global-set-key (kbd "M-p") 'clipboard-yank) ;; paste from clipboard
+(global-set-key (kbd "M-;") 'comment-line) ;; rebind add comment key combo
+
+;; smart openline
+(defun smart-open-line-below (arg)
+  "Insert an empty line after the current line.
+Position the cursor at its beginning, according to the current mode.
+   With a prefix ARG open line above the current line."
+  (interactive "P")
+  (if arg
+      (smart-open-line-above)
+    (progn
+      (move-end-of-line nil)
+      (newline-and-indent))))
+
+(defun smart-open-line-above ()
+  "Insert an empty line above the current line.
+Position the cursor at it's beginning, according to the current mode."
+  (interactive)
+  (move-beginning-of-line nil)
+  (newline-and-indent)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key (kbd "C-o") 'smart-open-line-below)
+(global-set-key (kbd "M-o") 'smart-open-line-above)
+
+;; highlight redundant whitespace
+(add-hook 'prog-mode-hook (lambda ()
+                            (interactive)
+                            (setq show-trailing-whitespace 1)))
